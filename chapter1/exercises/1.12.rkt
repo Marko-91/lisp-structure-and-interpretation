3e2e01c3218dc4eab032177df4ee5a558588872e
Marko Stankovic <marko.stankovic@clarivate.com>
2024-12-10 12:31:06 +0100

    refactor pascal triangle implementation for clarity and consistency

--- chapter1/exercises/1.12.rkt
+++ chapter1/exercises/1.12.rkt
@@ -1,17 +1,22 @@
 #lang racket
 
-(define (pascal row index)
+(define (pascal_generate row index)
 
   (define (calculate-number)
     (if (or (= index 0) (= index row))
         1
-        (+ (pascal (- row 1) index) (pascal (- row 1) (- index 1)))))
+        (+ (pascal_generate (- row 1) index) (pascal_generate (- row 1) (- index 1)))))
 
   (if (= row 5)
       -1 ;artifical break
-      ((if (= row index)
-           (pascal (+ row 1) 0)
-           (calculate-number))
-       (pascal row index))))
+      (if (= row index)
+          (pascal_generate (+ row 1) 0)
+          (calculate-number))))
 
-(pascal 0 0)
+(define (pascal row index)
+  (if (or (= index 0) (= index row))
+      1
+      (+ (pascal (- row 1) index) (pascal (- row 1) (- index 1)))))
+
+;(pascal 0 0)
+(pascal 3 3)
